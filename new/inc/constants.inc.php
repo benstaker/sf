5a095c616f8eab03a10200d3eb8c007304b73400
Benjamin Staker <bassatcollege@gmail.com>
2013-03-07 16:46:57 +0000

    Added 127.0.0.1 fix.

--- new/inc/constants.inc.php
+++ new/inc/constants.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-define("DB_SERVER", "localhost"); // Database Server.
+define("DB_SERVER", "127.0.0.1"); // Database Server.
 define("DB_USER", "root"); // Database Username.
 define("DB_PASS", ""); // Database Password.
 define("DB_DATABASE", "sf"); // Database name.
